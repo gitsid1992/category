99eeb5b153cf3fb3cf70ddab0577a3fca77d2cf4
bhavik0111 <118987227+bhavik0111@users.noreply.github.com>
2022-11-25 10:08:21 +0530

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php 
+
+include "connect.php"; 
+
+if (isset($_GET['id'])) 
+{
+    $user_id = $_GET['id'];
+
+    $sql = "DELETE FROM `category` WHERE `id`='$user_id'";
+
+     $result = $conn->query($sql);
+
+     if ($result == TRUE) 
+     {
+        echo "Record deleted successfully.";
+        header("location:view.php");
+     }
+     else
+     {
+        echo "Error:" . $sql . "<br>" . $conn->error;
+     }
+} 
+
+?>
